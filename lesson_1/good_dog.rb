1b852605fd27d76d5c86f5dc05ab339ae39f7e42
John Knotts <jhnnyk@gmail.com>
2016-04-05 10:27:45 -0600

    the to_s method with the GoodDog example

--- lesson_1/good_dog.rb
+++ lesson_1/good_dog.rb
@@ -1,18 +1,20 @@
 class GoodDog
-  @@number_of_dogs = 0
+  DOG_YEARS = 7
 
-  def initialize
-    @@number_of_dogs += 1
+  attr_accessor :name, :age
+
+  def initialize(n, a)
+    self.name = n
+    self.age = a * DOG_YEARS
   end
 
-  def self.total_number_of_dogs
-    @@number_of_dogs
+  def to_s
+    "This dog's name is #{name} and it is #{age} in dog years."
   end
 end
 
-puts GoodDog.total_number_of_dogs # => 0
-
-dog1 = GoodDog.new
-dog2 = GoodDog.new
-
-puts GoodDog.total_number_of_dogs # => 2
+sparky = GoodDog.new("Sparky", 4)
+puts sparky.age # => 28
+puts sparky
+p sparky
+puts "#{sparky}"
