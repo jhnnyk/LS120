646fe9bcd207b89c931a28a26870cf77bf2b1e9c
John Knotts <jhnnyk@gmail.com>
2016-04-05 18:08:06 -0600

    exercise 3

--- lesson_2/classes_and_objects.rb
+++ lesson_2/classes_and_objects.rb
@@ -2,13 +2,23 @@ class Person
   attr_accessor :first_name, :last_name
 
   def initialize(full_name)
-    parts = full_name.split
-    @first_name = parts.first
-    @last_name = parts.size > 1 ? parts.last : ''
+    parse_full_name(full_name)
   end
 
   def name
     "#{first_name} #{last_name}".strip
+  end
+
+  def name=(full_name)
+    parse_full_name(full_name)
+  end
+
+  private
+
+  def parse_full_name(full_name)
+    parts = full_name.split
+    self.first_name = parts.first
+    self.last_name = parts.size > 1 ? parts.last : ''
   end
 end
 
@@ -18,3 +28,7 @@ p bob.first_name
 p bob.last_name
 bob.last_name = 'Smith'
 p bob.name
+
+bob.name = "John Adams"
+p bob.first_name
+p bob.last_name
